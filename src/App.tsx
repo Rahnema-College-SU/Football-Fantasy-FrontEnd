540b99de4aef794d65058a43d3b4bb7476cafdd8
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2022-08-21 15:45:23 +0430

    Header template added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { BrowserRouter ,Route, Routes } from 'react-router-dom';
-import { Header } from './Header';
+import {Header} from './components/Header/Header';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+        </div>
+    );
 }
 
 export default App;
