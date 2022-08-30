f3e95742c95d6ba11e7c2018c516513ecdb7a5b1
shakiba.anarakifirooz <shakiba.anarakifirooz@gmail.com>
2022-08-30 18:32:33 +0430

    task5done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 
+import {Header} from './components/header/Header';
+import { RemainingMoney } from './components/remainingMoney/RemainingMoney';
+import { RemainingPlayer } from './components/remainingPlayer/remainingPlayer';
+import MiddleTabBar from './components/middleTabBar/ middleTabBar';
+import ChoosePlayer from './components/choosePlayer/choosePlayer';
+import DateBox from './components/dateBox/dateBox';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/*<img src={} className="App-logo" alt="logo" />*/}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <RemainingPlayer />
+            <RemainingMoney />
+            <MiddleTabBar />
+            <ChoosePlayer />
+            <DateBox/>
+            
+        </div>
+    );
+
 }
 
 export default App;
