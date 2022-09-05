9511c0463912c251a6027859c41d60a8a159c0df
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2022-09-05 17:05:57 +0430

    Task completed, not responsive, without animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Header from './components/header/Header';
 import NavigationBar from './components/navigationBar/NavigationBar';
+import {Route, Routes, useNavigate} from "react-router-dom";
+import MyTeam from "./components/myTeam/MyTeam";
+import Transfer from "./components/transfer/Transfer";
 
 function App() {
+    const navigate = useNavigate()
+
+    //TODO: do not change url when refresh page
+    useEffect(() => {
+        navigate('/my-team')
+    }, [])
+
     return (
         <div>
-            <Header />
-            <NavigationBar />
+            <Header/>
+            <NavigationBar/>
+
+            <Routes>
+                <Route path={'/my-team'} element={<MyTeam/>}/>
+                <Route path={'/transfers'} element={<Transfer/>}/>
+            </Routes>
         </div>
     );
 }
--- src/components/navigationBar/NavigationBar.tsx
+++ src/components/navigationBar/NavigationBar.tsx
@@ -1,67 +1,34 @@
-import React, {useCallback, useState} from 'react';
+import React, {useState} from 'react';
 import './NavigationBar.css';
+import {Link, useNavigate} from "react-router-dom";
 
 function NavigationBar() {
     const tabs = [
-        {id: 1, name: 'تیم من'},
-        {id: 2, name: 'نقل و انتقالات'},
-        {id: 3, name: 'آخرین رویدادها'},
-        {id: 4, name: 'پروفایل'},
-        {id: 5, name: 'جوایز'}
+        {id: 1, name: 'تیم من', urlName: 'my-team'},
+        {id: 2, name: 'نقل و انتقالات', urlName: 'transfers'},
+        {id: 3, name: 'آخرین رویدادها', urlName: 'latest-events'},
+        {id: 4, name: 'پروفایل', urlName: 'profile'},
+        {id: 5, name: 'جوایز', urlName: 'prizes'}
     ]
 
+    const [selectedTab, setSelectedTab] = useState(1)
+    const navigate = useNavigate()
 
     return (
-        <Navbar  selectedId={1} onChange={(id) => console.log(id)}>
-            <NavbarItem id={1}>تیم من</NavbarItem>
-            <NavbarItem id={2}>نقل ، انتقلات</NavbarItem>
-        </Navbar>
+        <div id={'nav-bar-div'}>
+            <ul id={'nav-bar-list'}>
+                {tabs.map(tab => (
+                    <li key={tab.id} onClick={() => {
+                        setSelectedTab(tab.id);
+                        navigate(tab.urlName)
+                    }
+                    } id={selectedTab === tab.id ? 'nav-bar-selected' : ''}>
+                        <Link id={selectedTab === tab.id ? 'a-selected' : '' } to={`/${tab.urlName}`}>{tab.name}</Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
 
-
-const NavbarContext = React.createContext<{selectedId: number, setSelectedId: (id: number) => void}>({selectedId: 0, setSelectedId: () => {}})
-
-
-const NavbarItem = ({id, children}: {id: number, children: React.ReactNode}) => {
-    const {selectedId, setSelectedId} = React.useContext(NavbarContext);
-     return  <li onClick={() => {
-         setSelectedId(id)
-     }} id={id === selectedId ? "nav-bar-selected": ""}>
-         <a id={id === selectedId ? "a-selected": ""}>{children}</a>
-     </li>
-
-}
-
-
-function Navbar({ selectedId, onChange, children}: { selectedId: number, onChange: (id: number) => void, children: React.ReactNode}) {
-
-    const [tab, setTab] = useState(selectedId);
-
-    const setSelectedId = useCallback((id: number) => {
-        onChange(id);
-        setTab(id);
-    }, [onChange]);
-    return (
-        <NavbarContext.Provider value={{selectedId: tab, setSelectedId}}>
-            <div id={'nav-bar-div'}>
-                <ul id={'nav-bar-table'}>
-                    {children}
-                </ul>
-            </div>
-        </NavbarContext.Provider>
-    );
-
-    function getLiId(tabId: number): string | undefined {
-        if (tab === tabId) {
-            return 'nav-bar-selected';
-        }
-    }
-
-    function getAId(tabId: number): string | undefined {
-        if (tab === tabId) {
-            return 'a-selected';
-        }
-    }
-}
 export default NavigationBar;
