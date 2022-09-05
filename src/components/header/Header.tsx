5d70147450bfa0a934e3d8c49320d0a851b21ac5
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2022-09-05 18:34:30 +0430

    Cleaner code

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,8 +7,9 @@ import logo from './assets/logo.svg'
 function Header() {
     return (
         <div className={"main-div"}>
-            <img className={"three-players-image"} src={fourPlayersImage} alt="Three football players" />
-            <img className={"curve-lines"} src={curveLines} alt="curve lines which related to premier league. It's somehow the second logo" />
+            <img className={"three-players-image"} src={fourPlayersImage} alt="Three football players"/>
+            <img className={"curve-lines"} src={curveLines}
+                 alt="curve lines which related to premier league. It's somehow the second logo"/>
             <div className={"logo-container"}>
                 <img className={"logo-image"} src={logo} alt="premier league logo"/>
                 <p className={"logo-text"}>فوتبال فانتزی</p>
