6f681ea5e0b20ee250b220f51bb10e2fa8cb257c
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2022-09-06 05:58:38 +0430

    Add ground image

--- src/components/home/myTeam/ground/Ground.tsx
+++ src/components/home/myTeam/ground/Ground.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Ground.css'
+import groundImage from './assets/ground.svg'
 
 function Ground() {
     return (
         <div id={'main-div'}>
+            <img src={groundImage} alt={'soccer ground'}/>
         </div>
     )
 }
