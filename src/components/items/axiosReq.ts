f6b88a0e18fbf9f611c77de6ccb35a6a231122e7
shakiba.anarakifirooz <shakiba.anarakifirooz@gmail.com>
2022-09-10 15:31:20 +0000

    Resolve "make the main window responsive"

---
+++ src/components/items/axiosReq.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+ 
+const http=axios.create(
+    {
+        baseURL:"http://178.216.248.39:8000",
+        timeout:5000,
+        headers:{ "Content-Type":"application/json",}
+    }
+);
+
+export default http;
