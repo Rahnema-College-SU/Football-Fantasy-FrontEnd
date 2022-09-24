507c42cdf2f52c894faed22d235d81f7f7dd749d
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2022-09-24 16:08:40 +0330

    Arrow keys bug fixed

--- src/global/Variables.ts
+++ src/global/Variables.ts
@@ -23,8 +23,8 @@ export const gkPositions = [1, 2]
 export const defPositions = [3, 4, 5, 6, 7]
 export const midPositions = [8, 9, 10, 11, 12]
 export const attPositions = [13, 14, 15]
-export const firstPosition = gkPositions[0]
-export const lastPosition = attPositions[-1]
+export const firstPosition = gkPositions.at(0)
+export const lastPosition = attPositions.at(-1)
 
 //They should have the same order as the tabs in the home page
 export const positionsServer = ['ALL', 'GKP', 'DEF', 'MID', 'FWD'] as const
