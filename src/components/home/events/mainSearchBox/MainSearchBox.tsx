1d50e5bba8e0f0cccb45a2a604edee054b80d252
shakiba.anarakifirooz <shakiba.anarakifirooz@gmail.com>
2022-09-28 19:17:42 +0330

    initialize input

--- src/components/home/events/mainSearchBox/MainSearchBox.tsx
+++ src/components/home/events/mainSearchBox/MainSearchBox.tsx
@@ -2,12 +2,21 @@ import React from "react";
 import "./MainSearchBox.css"
 import searchIcon from "./assets/searchicon.png";
 
+// export const usersListState = atom<usersListType>({
+//     key: 'usersListState',
+//     default: {
+//         usersList: [],
+//         //add number of users
+//     }
+// })
+
 export function MainSearchBox() {
     return (
-        <div className="searchBox">
-            <img src={searchIcon} alt={"magnifier"}/>
-            <text className="searchHint">اسم دوستات رو جستجو کن و دنبالشون کن</text>
-        </div>
+        <div className={'main-search-box'}>
+        <img className={'main-search-icon'} src={searchIcon} alt={'magnifier'}/>
+        <input className={'main-search-input'} placeholder={'اسم دوستات رو جستجو کن و دنبالشون کن'} />
+
+</div>
     )
 }
 
