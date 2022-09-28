8953d92e24e4d1aab55b4db723f7f4ad9aa27382
shakiba.anarakifirooz <shakiba.anarakifirooz@gmail.com>
2022-09-28 20:02:00 +0330

    updateing items for start

--- src/components/home/events/mainSearchBox/MainSearchBox.tsx
+++ src/components/home/events/mainSearchBox/MainSearchBox.tsx
@@ -10,13 +10,21 @@ import searchIcon from "./assets/searchicon.png";
 //     }
 // })
 
+const usersList=[
+    {id:"sh"},{id:"q"},{id:"a"}
+]
+
 export function MainSearchBox() {
     return (
+        <div>
         <div className={'main-search-box'}>
         <img className={'main-search-icon'} src={searchIcon} alt={'magnifier'}/>
         <input className={'main-search-input'} placeholder={'اسم دوستات رو جستجو کن و دنبالشون کن'} />
-
-</div>
+        </div>
+        <select className="main-search-result">
+        <option value=""> کشور</option>
+        </select>
+        </div>
     )
 }
 
