03c70a134c48b7ab583df8e91af1a8d52ac792c0
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2022-10-23 22:17:50 +0000

    Resolve "Prizes: coming soon"

--- src/components/home/prizes/Prizes.tsx
+++ src/components/home/prizes/Prizes.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import './Prizes.css';
+import comingSoon from './assets/coming-soon.gif'
+import winnerCup from './assets/winner-cup.svg'
 
 function Prizes() {
     return (
-        <div>
-            <h1>Prizes</h1>
+        <div id={'prizes-main-div'}>
+            <img id={'prizes-winner-cup-image'} src={winnerCup} alt={'winner cup'}/>
+            <img id={'prizes-coming-soon-image'} src={comingSoon} alt={'coming soon'}/>
         </div>
     )
 }
